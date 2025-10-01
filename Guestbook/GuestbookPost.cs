e2344814a130cfcba32e28adce3c1845e83f65c0
Tommy Issa <tommyissa@hotmail.com>
2025-10-01 22:03:55 +0200

    Har skapat en class som innehåller ägare samt text för inlägget.

---
+++ Guestbook/GuestbookPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guestbook
+{
+    internal class GuestbookPost
+    {
+        public string? Owner { get; set; }
+        public string? Post { get; set; }
+    }
+}
